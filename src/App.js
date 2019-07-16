82409d7ea38a25d24dd0c07cc87476525c58414c
ciemna_noc <agarkowa@gmail.com>
2019-07-16 22:05:22 +0300

    dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Footer } from './components/Footer/Footer';
+import { Navigation } from './components/Navigation/Navigation';
+import { Main } from './components/Main/Main';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-       Hello world!
+      <Navigation />
+      <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const Main = () => (
+  <div>Content</div>
+);
+
