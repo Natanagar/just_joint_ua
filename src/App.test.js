2a446cfc3d33bd694460b83826b9c191b7a8439b
ciemna_noc <agarkowa@gmail.com>
2019-07-16 22:13:42 +0300

    snapshot testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import Enzyme, { shallow, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
+// configuration Enzyme
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('component App should render without crashing', () => {
+  it('render without crashing', () => {
+    const component = shallow(<App />);
+    expect(component).toMatchSnapshot();
+  });
+});
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
