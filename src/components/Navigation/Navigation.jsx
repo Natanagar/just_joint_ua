531e44653f3b6017b3a30e2bfbb7a1999099cae5
ciemna_noc <agarkowa@gmail.com>
2019-07-17 20:55:06 +0300

    navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,16 +1,44 @@
 import React from 'react';
 
 export const Navigation = () => (
-        <div>
-            <nav>
-                <div>Logo</div>
-                <ul>
-                    <li>Career</li>
-                    <li>Hiring</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-
-
+  <React.Fragment>
+    <div className="parent">
+      <div className="navbar">
+        <div className="logo">
+          <picture>
+            <img src="http://fillmurray.com/50/50" alt="logo" />
+          </picture>
         </div>
-    );
+        <div className="menu">
+          <ul>
+            <li>
+              <a href="#jobs">Job offers</a>
+            </li>
+            <li>
+              <a href="#stories">Brand stories</a>
+            </li>
+            <li>
+              <a href="#geek">Just Geek IT</a>
+            </li>
+            <li>
+              <a href="#events">Events</a>
+            </li>
+            <li>
+              <a href="#about">About us</a>
+            </li>
+            <li>
+              <a href="#post">Post a job</a>
+            </li>
+            <li>
+              <a href="#registration">Registration</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div className="wrapper">
+      <div style={{ height: '2000px' }} />
+    </div>
+  </React.Fragment>
+);
