9e84ad6280c039baaddddff9413d44e72b59bb3c
ciemna_noc <agarkowa@gmail.com>
2019-07-26 02:28:07 +0300

    add comparison to leaflet

--- src/store/index.js
+++ src/store/index.js
@@ -15,16 +15,6 @@ export const JSOGTransform = createTransform(
 
   (inboundState, key) => JSOG.encode({ ...inboundState }),
   (outboundState, key) => JSOG.decode({ ...outboundState }),
-  // transform state on its way to being serialized and persisted.
-  /* (inboundState, key) =>
-    // convert mySet to an Array.
-    ({ ...inboundState, mySet: [...inboundState.mySet] }),
-  // transform state being rehydrated
-  (outboundState, key) =>
-    // convert mySet back to a Set.
-    ({ ...outboundState, mySet: new Set(outboundState.mySet) }),
-  // define which reducers this transform gets called for.
-  { whitelist: [rootReducer] }, */
 );
 
 export const persistConfig = {
